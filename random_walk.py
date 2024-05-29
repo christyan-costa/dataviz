d9737930168600c2e32d07d533a86599f1c8ebc1
chris <christyancosta@gmail.com>
2024-05-28 22:59:31 -0300

    creating method fill_walk()

--- random_walk.py
+++ random_walk.py
@@ -10,3 +10,29 @@ class RandomWalk():
         # All walks start at the origin (0,0).
         self.x_values = [0]
         self.y_values = [0]
+
+    def fill_walk(self):
+        """Calculate all the points in the walk."""
+
+        # Keep taking steps until walk reaches num_points 
+        while len(self.x_values) < self.num_points:
+            # Choose a direction and a magnitude for displacement along x-axis
+            x_direction = choice([1,-1])
+            x_distance = choice([0, 1, 2, 3, 4])
+            x_step = x_direction * x_distance
+            
+            # Choose a direction and a magnitude for displacement along y-axis
+            y_direction = choice([1,-1])
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
+
+            # Reject moves that go nowhere 
+            if x_step == 0 and y_step == 0:
+                continue 
+
+            # Calculate the next x and y values 
+            next_x = self.x_values[-1] + x_step
+            next_y = self.y_values[-1] + y_step 
+
+            self.x_values.append(next_x)
+            self.y_values.append(next_y)
