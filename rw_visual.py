9e9114cfacf17d3bd930f0aef6d19faa7486bf42
chris <christyancosta@gmail.com>
2024-05-29 23:52:08 -0300

    changing from plt.scatter to plt.plot

--- rw_visual.py
+++ rw_visual.py
@@ -5,16 +5,14 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active.
 while True:
     # Make a random walk.
-    rw = RandomWalk(100_000)
+    rw = RandomWalk(5_000)
     rw.fill_walk()
 
     # Set the size of the plotting window.
     plt.figure(figsize=(10, 6))
     # Plot the points in the random walk.
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
-                edgecolor='none',
-                s=1)
+    plt.plot(rw.x_values, rw.y_values, linewidth=0.5)
     # Emphasize the first and last points.
     plt.scatter(0,0, c='green', edgecolors='none', s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
