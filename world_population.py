4660dfac3fea85077db18d792e9a77899d0a77d8
chris <christyancosta@gmail.com>
2024-05-31 22:13:16 -0300

    converting strings into numerical values

--- world_population.py
+++ world_population.py
@@ -5,9 +5,9 @@ filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
 
-# Print the population for each country.
+# Print the 2010 population for each country.
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
-        population = pop_dict['Value']
-        print(country_name + ": " + population)
+        population = int(float(pop_dict['Value']))
+        print(country_name + ": " + str(population))
